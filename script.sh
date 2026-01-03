63a008d8e02e4d621045509684cd5d89102a3ee2
nvminh162 <nvminh162@gmail.com>
2026-01-03 11:57:17 +0700

    remove CloudBeaver setup from script.sh

--- script.sh
+++ script.sh
@@ -7,9 +7,3 @@ docker run --name neo4j --publish=7474:7474 --publish=7687:7687 -d neo4j:latest
 # mysql docker
 docker pull mysql:8.0.44-debian
 docker run --name mysql -p 3306:3306 -v mysql_data:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=root -d mysql:8.0.44-debian
-# tool browser:
-docker pull dbeaver/cloudbeaver:latest
-docker run --name cloudbeaver -p 8978:8978 dbeaver/cloudbeaver:latest
-
-
-

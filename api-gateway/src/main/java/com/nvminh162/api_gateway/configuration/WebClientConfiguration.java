5c9b2bbf81ffaf4e89ab1fd1017d25e22035af70
nvminh162 <nvminh162@gmail.com>
2026-01-04 11:49:51 +0700

    Add CORS support in WebClientConfiguration for cross-origin requests

--- api-gateway/src/main/java/com/nvminh162/api_gateway/configuration/WebClientConfiguration.java
+++ api-gateway/src/main/java/com/nvminh162/api_gateway/configuration/WebClientConfiguration.java
@@ -3,9 +3,14 @@ package com.nvminh162.api_gateway.configuration;
 import com.nvminh162.api_gateway.repository.IdentityClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.support.WebClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import java.util.List;
 
 // đối với http của spring 6 phải khai báo thêm webclient => register này => http serivice proxy => nó mới implement cho chúng ta
 @Configuration
@@ -15,6 +20,19 @@ public class WebClientConfiguration {
         return WebClient.builder()
                 .baseUrl("http://localhost:8080/identity")
                 .build();
+    }
+
+    @Bean
+    CorsWebFilter corsWebFilter() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedOrigins(List.of("*"));
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource  source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+
+        return new CorsWebFilter(source);
     }
 
     /*

a2d7b17b890996e8b2092bcc1a4b11aad071d70f
nvminh162 <nvminh162@gmail.com>
2026-01-04 16:40:41 +0700

    Initialize `notification-service` project with Spring Boot setup, MongoDB configuration, exception handling, and Maven wrapper files.

--- script.sh
+++ script.sh
@@ -7,3 +7,8 @@ docker run --name neo4j --publish=7474:7474 --publish=7687:7687 -d neo4j:latest
 # mysql docker
 docker pull mysql:8.0.44-debian
 docker run --name mysql -p 3306:3306 -v mysql_data:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=root -d mysql:8.0.44-debian
+
+# mongodb docker
+# https://hub.docker.com/r/bitnami/mongodb/tags
+docker pull bitnami/mongodb:latest
+docker run -d --name mongodb-7.0.11 -p 27017:27017 -e MONGODB_ROOT_USER=root -e MONGODB_ROOT_PASSWORD=root bitnami/mongodb:latest

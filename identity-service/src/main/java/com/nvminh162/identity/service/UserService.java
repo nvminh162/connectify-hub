05aeec6e1e35870a690d75a567eaf1b8c57d6fae
nvminh162 <nvminh162@gmail.com>
2026-01-09 23:43:17 +0700

    Remove Maven wrapper and related configuration files

---
+++ identity-service/src/main/java/com/nvminh162/identity/service/UserService.java
@@ -0,0 +1,57 @@
+package com.nvminh162.identity.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.nvminh162.identity.dto.request.UserCreationRequest;
+import com.nvminh162.identity.dto.request.UserUpdateRequest;
+import com.nvminh162.identity.entity.User;
+import com.nvminh162.identity.repository.UserRepository;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Service
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class UserService {
+    UserRepository userRepository;
+
+    public User createUser(UserCreationRequest request) {
+        User user = new User();
+
+        user.setUsername(request.getUsername());
+        user.setPassword(request.getPassword());
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setDob(request.getDob());
+
+        return userRepository.save(user);
+    }
+
+    public User updateUser(String userId, UserUpdateRequest request) {
+        User user = getUser(userId);
+
+        user.setPassword(request.getPassword());
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setDob(request.getDob());
+
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(String userId) {
+        userRepository.deleteById(userId);
+    }
+
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUser(String id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+    }
+}
